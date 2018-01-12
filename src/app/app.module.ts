b33ab797d1734cd54aaf3fb6dd8dc30aa62d7518
devroji <roj_jos@hotmail.com>
2018-01-11 19:20:31 -0500

    created all the components home aboutus contactus header footerand installed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'Home',
+        component: HomeComponent
+      },
+      { path: 'AboutUs',
+        component: AboutUsComponent
+      },
+      { path: 'ContactUs',
+        component: ContactUsComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
